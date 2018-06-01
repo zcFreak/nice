d6685e5395dd7182ffa45e841eef01374527dc3a
zcFreak <1056091538@qq.com>
2018-06-01 11:19:07 +0800

    zc

--- application/controllers/Admin/Index.php
+++ application/controllers/Admin/Index.php
@@ -3,7 +3,7 @@
 class Index extends CI_Controller
 {
 
-    public function __construct()
+    function __construct()
     {
         parent::__construct();
         $this->load->model('Admin/Index_model', "index");
