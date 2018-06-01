48799ea53f56116fda10f3c543597a33c8b3669c
zcFreak <1056091538@qq.com>
2018-06-01 11:32:52 +0800

    zc

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','pagination','user_agent', 'Common');
+$autoload['libraries'] = array('database','session','pagination','user_agent');
 
 /*
 | -------------------------------------------------------------------
